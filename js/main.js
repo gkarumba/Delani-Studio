b4a4e6db7d4db3ba67cf6541c7fec1b637159136
gkarumba <gachegua@gmail.com>
2019-05-04 22:38:02 +0300

    Add the jQuery to allow for hovering over portfolio

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
 $(document).ready(function (){
     //WHAT WE DO SECTION 
-    $("#design").click(function (){
-        $("#design").hide();
-        $("#design-info").show();
-    });
-    $("#design-info").click(function (){
-        $("#design-info").hide();
-        $("#design").show();
-    });
-    $("#development").click(function (){
-        $("#development").hide();
-        $("#development-info").show();
-    });
-    $("#development-info").click(function (){
-        $("#development-info").hide();
-        $("#development").show();
-    });
-    $("#product").click(function (){
-        $("#product").hide();
-        $("#product-info").show();
-    });
-    $("#product-info").click(function (){
-        $("#product-info").hide();
-        $("#product").show();
+    var whatWeDo = ['design','development','product'];
+    whatWeDo.forEach(function(what){
+        $("#"+what).click(function (){
+            $("#"+what).hide();
+            $("#"+what+"-info").show();
+        });
+        $("#"+what+"-info").click(function (){
+            $("#"+what+"-info").hide();
+            $("#"+what).show();
+        });
     });
     // PORTFOLIO SECTION
-    ("#work1").hover(function () {
-        $("#work-p").show();
+    var workIds = ['work1','work2','work3','work4','work5','work6','work7','work8'];
+    workIds.forEach(function(id){
+        $("#"+id).mouseover(function () {
+            $("."+id+"-div").show();
+        });
+        $("#"+id).mouseout(function () {
+            $("."+id+"-div").hide();
+        });
     });
-    ("#work1").mouseleave(function () {
-        $("#work-p").hide();
-    });
-})
+});

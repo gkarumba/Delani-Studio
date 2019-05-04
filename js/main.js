3d80c3f7c9836b95ea5036dcde674aa55bcd52dc
gkarumba <gachegua@gmail.com>
2019-05-04 18:34:51 +0300

    Add the jQuery to hide and show the design,development and product info divs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-// $(document).ready(function (){
-//     $(#design).
-// })
+$(document).ready(function (){
+    $("#design").click(function (){
+        $("#design").hide();
+        $("#design-info").show();
+    });
+    $("#design-info").click(function (){
+        $("#design-info").hide();
+        $("#design").show();
+    });
+    $("#development").click(function (){
+        $("#development").hide();
+        $("#development-info").show();
+    });
+    $("#development-info").click(function (){
+        $("#development-info").hide();
+        $("#development").show();
+    });
+    $("#product").click(function (){
+        $("#product").hide();
+        $("#product-info").show();
+    });
+    $("#product-info").click(function (){
+        $("#product-info").hide();
+        $("#product").show();
+    });
+})
